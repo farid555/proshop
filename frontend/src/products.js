a65894ee380ea2b6e94af627e265b437896d280e
farid555 <mohammad.uzzaman2@centria.fi>
2021-01-15 20:40:04 +0200

    added Products

---
+++ frontend/src/products.js
@@ -0,0 +1,81 @@
+const products = [{
+    _id: '1',
+    name: 'Airpods wireless Bluethoot Headphone',
+    image: '/images/airpods.jpg',
+    descripation: 'Airpods wireless Bluethoot Headphone',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.88,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 9,
+
+},
+{
+    _id: '2',
+    name: 'Toyota',
+    image: '/images/alex.jpg',
+    descripation: 'Even in the age of next-day delivery, self-checkout, and gigabit internet, some things can not be rushed. ',
+    brand: 'Toyota',
+    category: 'Electronics',
+    price: 300.88,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 6,
+
+},
+{
+    _id: '3',
+    name: 'Sony wx350',
+    image: '/images/camera.jpg',
+    descripation: 'The best Sony cameras come in all shapes and sizes, from high-speed sports shooters! ',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 89.88,
+    countInStock: 3,
+    rating: 5.5,
+    numReviews: 3,
+
+},
+{
+    _id: '4',
+    name: 'JBL Bluethoot Headphone',
+    image: '/images/headphone.jpg',
+    descripation: 'Go to Settings and turn on Bluetooth. In Bluetooth click “Pair new device',
+    brand: 'JBL',
+    category: 'Electronics',
+    price: 150.88,
+    countInStock: 5,
+    rating: 6.5,
+    numReviews: 8,
+
+},
+{
+    _id: '5',
+    name: 'lamp',
+    image: '/images/lamp.jpg',
+    descripation: 'life time longerbity',
+    brand: 'PHILLIPS',
+    category: 'Electronics',
+    price: 15.88,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 6,
+
+},
+{
+    _id: '6',
+    name: 'Apple Watch',
+    image: '/images/watch.jpg',
+    descripation: 'Apple Watch...',
+    brand: 'Apple watch',
+    category: 'Electronics',
+    price: 89.88,
+    countInStock: 3,
+    rating: 2.5,
+    numReviews: 4,
+
+
+}]
+
+export default products;
